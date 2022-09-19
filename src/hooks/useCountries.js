01759959f55423bb75657dab7882cae46f58ddde
Emily Pisaygnane <episaygnane@me.com>
2022-09-19 15:14:33 -0700

    created useCountries function with useEffect to call services function

---
+++ src/hooks/useCountries.js
@@ -0,0 +1,19 @@
+import { fetchCountries } from "../services/countries";
+import { useState, useEffect } from "react";
+
+export default function useCountries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCountries();
+      setCountries(data);
+    }
+  
+    fetchData();
+  
+  }, []);
+
+  return countries;
+
+}
